9bc8bc6ef90ba2b9fa16695fbcbb1cdd9e238852
Enrico Biancotto <enrico.biancotto@studenti.unipd.it>
2022-07-04 10:12:37 +0200

    Update Multiple components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,18 @@
-
-const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now);
-
+const Hello = () => {
   return (
     <div>
-      <p>
-        Hello World, it is {now.toString()}
-      </p>
-        {a} + {b} = {a + b}
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
     </div>
   );
 }
