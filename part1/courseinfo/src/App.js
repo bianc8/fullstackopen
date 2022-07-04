f716b9f9477acbdde3a604b0e6009d6ceca44005
Enrico Biancotto <enrico.biancotto@studenti.unipd.it>
2022-07-04 11:02:09 +0200

    Update step1/courseinfo step 5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -38,28 +38,39 @@ const Total = (props) => {
     </>
   )
 }
+
+const Course = (props) => {
+  return (
+    <>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Course course={course} />
     </div>
   )
 }
