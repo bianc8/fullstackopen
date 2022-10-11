7d7507f2c329e5810cfde558a7827c053013037b
Enrico Biancotto <enrico.biancotto@studenti.unipd.it>
2022-10-11 20:22:47 +0200

    Add lecture 8.c

---
+++ lessons/part8/backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+/*
+For simplicity's sake, let's assume that all users have the same password which is hardcoded to the system.
+The idea is that when a user, e.g. mluukkai, adds a person, e.g. Arto Hellas, to the list, the person is added to their friends list.
+*/
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Person'
+    }
+  ],
+})
+
+module.exports = mongoose.model('User', schema)
